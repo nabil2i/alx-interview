843284ffab6ccd2018bdf852b2e3bb9736c6c2cb
Nabil <nabil.noble01@gmail.com>
2023-03-02 18:26:10 +0000

    add function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-"""
-function to determine the permiter of an island
-"""
+"""module for function island_perimeter(grid)
+to determine the permiter of an island"""
 
 
 def island_perimeter(grid):
-  """returns the perimeter of the island described in grid
-  """
+    """returns the perimeter of the island described in grid"""
+    perimeter = 0
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == 1:
+                if y == 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                    perimeter += 1
+                if x == 0 or grid[y][x - 1] == 0:
+                    perimeter += 1
+                if x == len(row) - 1 or grid[y][x + 1] == 0:
+                    perimeter += 1
+    return perimeter
